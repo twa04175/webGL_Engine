99e01b20ec7ec8a4e3c407aebfd8d20c92cc0e31
twa04175 <twa04175@naver.com>
2022-04-26 20:42:48 +0900

    initCommit

---
+++ src/engine.js
@@ -0,0 +1,20 @@
+let startObj = [];
+let updateObj = [];
+let enableObj = [];
+let disableObj = [];
+let destoryObj = [];
+
+let frame = () =>{
+
+}
+
+let running = () => {
+
+    let frame = 60;
+    let deltaTime = 1000 / frame;
+
+    setInterval(()=>{
+        this.frame();
+    },deltaTime);
+}
+
